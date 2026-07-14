224d84ca86b8fb095f2433a0166419672a61aa9d
me-yubik <yubik2016@gmail.com>
2026-07-14 18:58:58 +0545

    uploading on my branch for less confusion and please check the main if there is any unwanted update

---
+++ include/Message.h
@@ -0,0 +1,66 @@
+#ifndef MESSAGE_H
+#define MESSAGE_H
+
+#include "BaseEntity.h"
+#include <string>
+
+using namespace std;
+
+class Message : public BaseEntity {
+    private: 
+    int roomID; 
+    int senderID;
+    string senderName;
+    string content;
+    string timestamp;
+
+    public:
+    Message (int id, int roomID ,senderID, const string& senderName, const string& content, const string& timestamp) 
+        : BaseEntity(id), roomID(roomID), senderID(senderID), senderName(senderName), content(content), timestamp(timestamp) {}
+
+        int getRoomID const { return roomID; }
+        int getSenderID const { return senderID; }
+        string getSenderName const { return senderName; }
+        string getContent const { return content; }
+        string getTimestamp const { return timestamp; }
+
+        string toString () const override {
+            return "[" + timestamp +"] " + senderName + ": " + content; 
+        }
+};
+#endif 
+
+
+class Message : public BaseEntity {
+private:
+    int roomID;
+    int senderID;
+    std::string senderName;
+    std::string content;
+    std::string timestamp;
+
+public:
+    Message(int id, int roomID, int senderID,
+            const std::string& senderName,
+            const std::string& content,
+            const std::string& timestamp)
+        : BaseEntity(id),
+          roomID(roomID),
+          senderID(senderID),
+          senderName(senderName),
+          content(content),
+          timestamp(timestamp) {}
+
+    int getRoomID() const { return roomID; }
+    int getSenderID() const { return senderID; }
+    std::string getSenderName() const { return senderName; }
+    std::string getContent() const { return content; }
+    std::string getTimestamp() const { return timestamp; }
+
+    std::string toString() const override {
+        return "[" + timestamp + "] " + senderName + ": " + content;
+    }
+};
+
+#endif
+
---
+++ include/Room.h
@@ -0,0 +1,36 @@
+#ifndef ROOM_H
+#define ROOM_H
+
+#include "BaseEntity.h"
+#include "Message.h"
+#include <string>
+#include <vector>
+#include <memory>
+using namespace std;
+
+class Room : public BaseEntity {
+
+    private: 
+    string name;
+    int createdBy; 
+    vector<shared_ptr<Message>> messages;
+
+    public:
+    Room (int id , const string& name , int createdBy) : BaseEntity(id), name(name), createdBy(createdBy) {}
+
+    string getName() const { return name; }
+    int getCreatedBy() const { return createdBy; }
+    
+    void addMessage(shared_ptr<Message> message) {
+    messages.push_back(message);
+}
+    
+    vector<shared_ptr<Message>> getMessages() const {
+        return messages;
+    }
+
+    string toString() const override {
+        return "Room [" + to_string(id) + "]: " + name;
+    }
+};
+#endif
